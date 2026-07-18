f2f3d2b57d58bf177602a180c1c7954c5388bac8
surferPHP <crazylion.n@gmail.com>
2026-07-18 14:03:07 +0300

    Add Google Analytics tracking

---
+++ analytics.js
@@ -0,0 +1,8 @@
+window.dataLayer = window.dataLayer || [];
+
+window.gtag = function gtag() {
+  window.dataLayer.push(arguments);
+};
+
+window.gtag("js", new Date());
+window.gtag("config", "G-3N5ETPWH47");
